1aeef34e9f1feed0e29e5b972f95441f2c99b43a
shewittnwu <77414281+shewittnwu@users.noreply.github.com>
2022-06-21 14:06:49 -0400

    changed filepath again

--- assets/js/runner.js
+++ assets/js/runner.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 console.log('test log output');
 
-fs.readFile('/assets/js/data_events.json', 'utf8', (err, data) => {
+fs.readFile('/home/runner/work/chindaiko-web/chindaiko-web/assets/js/data_events.json', 'utf8', (err, data) => {
     if(err){
         console.log(err);
         return;
