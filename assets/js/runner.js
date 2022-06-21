4e526bf66d0af6e457088f839924914f4f781932
shewittnwu <77414281+shewittnwu@users.noreply.github.com>
2022-06-21 13:53:43 -0400

    added fs readfile to runner

--- assets/js/runner.js
+++ assets/js/runner.js
@@ -1,3 +1,10 @@
-
+const fs = require('fs');
 console.log('test log output');
 
+fs.readFile('data_events.json', 'utf8', (err, data) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(data);
+})
