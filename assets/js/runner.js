062bb63f746f2eeb188dff278f54eeb716fbfc4b
shewittnwu <77414281+shewittnwu@users.noreply.github.com>
2022-06-21 14:24:30 -0400

    added some basic notion api components, pretesting

--- assets/js/runner.js
+++ assets/js/runner.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
+const { Client } = require("@notionhq/client")
+
+// let eventsfilepath= '/home/runner/work/chindaiko-web/chindaiko-web/assets/js/data_events.json';
+let eventsfilepath ='data_events.json';
+
 console.log('test log output');
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  })
 
 fs.readFile('/home/runner/work/chindaiko-web/chindaiko-web/assets/js/data_events.json', 'utf8', (err, data) => {
     if(err){
